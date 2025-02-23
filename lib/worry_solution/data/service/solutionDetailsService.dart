1d55b24bec42bdba9d9d26992419d5594d62f61c
hyeminililo <ng040115@gmail.com>
2025-02-23 23:00:11 +0900

    #2 feat : 고민 상세보기 서비스 로직 작성

---
+++ lib/worry_solution/data/service/solutionDetailsService.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gomin_jungdok_mobile/worry_solution/data/model/solutionDetails_model.dart';
+import 'package:gomin_jungdok_mobile/worry_solution/data/repository/solutionDetails_repository.dart';
+
+class Solutiondetailsservice {
+  final SolutiondetailsRepository repository;
+  SolutionDetails solutionDetailsModel;
+  Solutiondetailsservice(this.repository, this.solutionDetailsModel);
+
+  Future<SolutionDetails> fetchSolutionDetails(int postId) async {
+    final response = await repository.fetchDetailsSolutionPosts(id: postId);
+
+    debugPrint("[PRINT_SOLUTION_DETAILS] : ${response.data.toString()}");
+    if (response.statusCode != 200) {
+      print("[ERROR] failed to fetch solutionDetails");
+    }
+
+    solutionDetailsModel = response.data!;
+    return solutionDetailsModel;
+  }
+}
