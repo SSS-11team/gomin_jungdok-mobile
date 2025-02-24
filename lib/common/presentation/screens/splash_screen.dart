b3147c7a9eb9efbd53a2cd9401bc20d7fb69e7a3
hyeminililo <ng040115@gmail.com>
2025-02-25 02:55:16 +0900

    #2 feat : splash screen UI 구현

--- lib/common/presentation/screens/splash_screen.dart
+++ lib/common/presentation/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +11,23 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: child);
+    final Size size = MediaQuery.of(context).size;
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: size.width * 0.55,
+            ),
+            SvgPicture.asset(
+              'assets/icons/logoTypo.svg',
+              height: size.width * 0.5,
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gomin_jungdok_mobile/common/presentation/screens/splash_screen.dart';
 import 'package:gomin_jungdok_mobile/worry_solution/presentation/screens/solutionDetails_screen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const SolutionDetailsView(),
+      home: const SplashScreen(),
     );
   }
 }
