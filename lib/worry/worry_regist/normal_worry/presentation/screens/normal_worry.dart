857a98b5ac8903046532898eaf6a6ce3a8d87091
hyeminililo <ng040115@gmail.com>
2025-03-26 21:56:12 +0900

    #8 refactor : 오늘의 고민 api 수정

--- lib/worry/worry_regist/normal_worry/presentation/screens/normal_worry.dart
+++ lib/worry/worry_regist/normal_worry/presentation/screens/normal_worry.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gomin_jungdok_mobile/common/const/api.dart';
