0914fd91dcb9cfca7aba3be6580d83263fefe405
hyeminililo <ng040115@gmail.com>
2025-03-27 13:17:35 +0900

    #8 feat : 오늘의 고민 공개 전/후 화면 적용

--- lib/worry/today_worry/presentation/screens/todayWorryList_screens.dart
+++ lib/worry/today_worry/presentation/screens/todayWorryList_screens.dart
@@ -1,55 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:gomin_jungdok_mobile/common/const/colors.dart';
-// import 'package:gomin_jungdok_mobile/worry/today_worry/presentation/screens/todayWorryDetails_screen.dart';
-// import 'package:gomin_jungdok_mobile/worry/today_worry/provider/todayWorry_prov.dart';
-
-// class TodayWorryListScreens extends ConsumerWidget {
-//   const TodayWorryListScreens({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final todayWorryPosts = ref.watch(fetchTodayWorryPostsProvider);
-
-//     return Scaffold(
-//       backgroundColor: MAIN_BG_COLOR,
-//       appBar: AppBar(
-//         title: const Text('오늘의 고민 목록'),
-//         backgroundColor: MAIN_BG_COLOR,
-//       ),
-//       body: todayWorryPosts.when(
-//         data: (todayWorryList) {
-//           return ListView.builder(
-//             itemCount: todayWorryList.length,
-//             itemBuilder: (context, index) {
-//               final post = todayWorryList[index];
-//               return ListTile(
-//                 title: Text(post.title),
-//                 subtitle: Text(post.description),
-//                 onTap: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                       builder: (context) =>
-//                           TodayWorryDetailsScreen(postId: post.postId),
-//                     ),
-//                   );
-//                 },
-//               );
-//             },
-//           );
-//         },
-//         loading: () => const Center(child: CircularProgressIndicator()),
-//         error: (error, stackTrace) => Center(child: Text('데이터를 불러오지 못했습니다.')),
-//       ),
-//     );
-//   }
-// }
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gomin_jungdok_mobile/common/const/colors.dart';
-import 'package:gomin_jungdok_mobile/common/presentation/router/go_router.dart';
 import 'package:gomin_jungdok_mobile/worry/today_worry/presentation/screens/todayWorryDetails_screen.dart';
 import 'package:gomin_jungdok_mobile/worry/today_worry/provider/todayWorry_prov.dart';
 
